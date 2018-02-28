665dc70c33a7c754d8f998908126e28c9794a4cb
joanca <jcaraven@thoughtworks.com>
2018-02-28 17:27:24 -0300

    #136 [JC] Se modifica functions.php para agregar post_meta_fields a respuesta de API

--- wp-content/themes/creative-gem/functions.php
+++ wp-content/themes/creative-gem/functions.php
@@ -1,5 +1,31 @@
 <?php
 /**
+* Start expose post meta fields in rest API
+*/
+add_action( 'rest_api_init', 'create_api_posts_meta_field' );
+ 
+function create_api_posts_meta_field() {
+ 
+ // register_rest_field ( 'name-of-post-type', 'name-of-field-to-return', array-of-callbacks-and-schema() )
+ register_rest_field( 'post', 'post-meta-fields', array(
+ 'get_callback' => 'get_post_meta_for_api',
+ 'schema' => null,
+ )
+ );
+}
+ 
+function get_post_meta_for_api( $object ) {
+ //get the id of the post object array
+ $post_id = $object['id'];
+ 
+ //return the post meta
+ return get_post_meta( $post_id );
+}
+/**
+* End expose post meta fields in rest API
+*/
+
+/**
  * creative Lite functions and definitions.
  *
  * @link https://developer.wordpress.org/themes/basics/theme-functions/
