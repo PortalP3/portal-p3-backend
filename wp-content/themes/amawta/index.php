b0c809113a6401edb7eccf26b916d095aae2763e
joanca <jcaraven@thoughtworks.com>
2018-03-16 16:36:33 -0300

    NOSTORY [JC] Eliminando redirección en header

--- wp-content/themes/amawta/index.php
+++ wp-content/themes/amawta/index.php
@@ -7,28 +7,4 @@
  * @since Amawta 1.0
  */
 
-  header('Location: '.getenv('FRONTEND_URL'));
  ?>
-
- <!DOCTYPE html>
-
-<html <?php language_attributes(); ?> class="no-js">
-   <head>
-       <meta charset="<?php bloginfo( 'charset' ); ?>">
-       <meta name="viewport" content="width=device-width">
-       <link rel="profile" href="http://gmpg.org/xfn/11">
-       <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
-       <title>Amawta</title>
-       <?php wp_head(); ?>
-   </head>
-   <body <?php body_class(); ?>>
-       <div id="page" class="hfeed site">
-           <div id="content">
-               <div class="loader-gif">
-                   <img src="<?php echo home_url() ?>/wp-content/themes/celestial/dist/images/loading-icon.gif" alt="Loader">
-               </div>
-           </div>
-           <?php wp_footer(); ?>
-       </div>			
-   </body>
-</html>
