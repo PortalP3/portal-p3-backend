4eacf165023490e324920d7ed2148f072c3b811f
joanca <jcaraven@thoughtworks.com>
2018-03-16 16:50:32 -0300

    NOSTORY [JC] Se cambia nombre a funciones en functions.php

--- wp-content/themes/amawta/functions.php
+++ wp-content/themes/amawta/functions.php
@@ -4,25 +4,25 @@
   function create_api_posts_meta_field() {
     // Add rating info (it's inside post meta fields)
     register_rest_field( 'post', 'post_meta_fields', array(
-      'get_callback' => 'get_post_meta_info',
+      'get_callback' => 'rest_get_post_meta',
       'schema' => null,
     ));
 
     // Add author name
     register_rest_field( 'post', 'author_name', array(
-      'get_callback'		=> 'get_author_name',
+      'get_callback'		=> 'rest_get_author_name',
       'update_callback'	=> null,
       'schema'			=> null
     ));
   }
   
-  function get_post_meta_info( $object ) {
+  function rest_get_post_meta( $object ) {
     $post_id = $object['id'];
     
     return get_post_meta( $post_id );
   }
 
-  function get_author_name( $object, $field_name, $request ) {
+  function rest_get_author_name( $object, $field_name, $request ) {
     return get_the_author_meta( 'display_name' );
   }
 ?>
